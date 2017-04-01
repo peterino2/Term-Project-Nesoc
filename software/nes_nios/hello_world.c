2a3f51d52a8594b2a78807ae096ecf73676402c0
sear-PC\sear <peterlimail47@gmail.com>
2017-04-01 16:30:46 -0700

    updated .gitignore to keep .c files

---
+++ software/nes_nios/hello_world.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+#include <stdlib.h>
+#include "altera_avalon_timer_regs.h"
+#include "altera_avalon_timer.h"
+#define TEST_TIMER_BASE 0x0200020 	// timer base
+#define FRAME_BUFF_SIZE  61440//240*256
+int main()
+{
+    printf("SDRAM ONLINE MY DUDE!\n");
+    void* frameBuffer =  malloc(61440);
+    void* secondMalloc = malloc(10000); // lets allocate 10 megabytes for funzies
+    int count = 0;
+    printf("aww shiet son we got malloc online : %08x\n", (unsigned int)frameBuffer);
+    printf("huge 10MB block at: %08x", (unsigned int)secondMalloc);
+    void* scanstart = (void*)0x02008000; // test to see how long it takes to copy 256 bytes from
+    // sdram to onchip
+    // onchip starts at 0x0200_8000 for 256 bytes ends at 0x0200_80FF
+    unsigned int i = 1;
+    unsigned int tv_0 = 0;	//timer value 0
+    unsigned int tv_1 = 0;	// timer value 1
+    while(1) {
+    	IOWR_ALTERA_AVALON_TIMER_SNAPL(TEST_TIMER_BASE,0);
+    	tv_0 = IORD_ALTERA_AVALON_TIMER_SNAPL(TEST_TIMER_BASE); // read from snap_l timer register
+    	for(i = 0;i< 256;i++){
+    		*(int*)(scanstart+i) = *(int*)(frameBuffer + i);
+    	}
+    	IOWR_ALTERA_AVALON_TIMER_SNAPL(TEST_TIMER_BASE,0);
+    	tv_1 = IORD_ALTERA_AVALON_TIMER_SNAPL(TEST_TIMER_BASE);// read from snap_l timer register
+    	printf("time taken to copy 256 Bytes (in us): %d\n", (tv_1 - tv_0) );
+    	IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE, count & 0x01);
+        count++;
+    }
+
+  return 0;
+}
